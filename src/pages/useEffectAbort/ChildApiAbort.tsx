bb90310322f637f904d42a84157fed1e9a7d068e
jeoniltae <foodchain@naver.com>
2026-03-15 00:02:58 +0900

    update: API성공 응답 처리 및 데이터 파싱하여 목록 랜더링 연습

--- src/pages/useEffectAbort/ChildApiAbort.tsx
+++ src/pages/useEffectAbort/ChildApiAbort.tsx
@@ -1,6 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+
+type product = {
+  id: number;
+  title: string;
+};
 
 const ChildApiAbort = () => {
+  const [products, setProducts] = useState<product[]>([]);
+
   useEffect(() => {
     console.log("자식 마운트");
 
@@ -11,6 +18,21 @@ const ChildApiAbort = () => {
     fetch("https://dummyjson.com/products?delay=2000", { signal: signal })
       .then((res) => {
         console.log("res: ", res);
+        return res.json();
+      })
+      .then((data) => {
+        // 만약 data.products가 undefined이면 .map()을 호출할 수 없어서 에러가 납니다. ?? []로 빈 배열을 기본값으로 넣어주면 .map()이 에러 없이 빈 결과를 반환하게 됩니다.
+        const productsData = data.products ?? [];
+        const products = productsData.map((data: product) => {
+          return {
+            id: data.id,
+            title: data.title,
+          };
+        });
+        console.log("==============");
+        console.log(products);
+        console.log("==============");
+        setProducts(products); // 👈 성공 응답 랜더링
       })
       .catch((err) => {
         if (err.name === "AbortError") {
@@ -35,6 +57,15 @@ const ChildApiAbort = () => {
   return (
     <div>
       <h1>ChildApiAbort</h1>
+      <ul>
+        {products.map((v) => {
+          return (
+            <li key={v.id}>
+              {v.id}. {v.title}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
